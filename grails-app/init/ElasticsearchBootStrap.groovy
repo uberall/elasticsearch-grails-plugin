2b9050a314d287f356717c63e78885029853ed46
Bruno <blopes@ufscar.br>
2017-06-14 16:18:59 -0300

    Inject dynamic methods in Bootstrap

--- grails-app/init/ElasticsearchBootStrap.groovy
+++ grails-app/init/ElasticsearchBootStrap.groovy
@@ -2,16 +2,22 @@
 
 import grails.plugins.elasticsearch.ElasticSearchBootStrapHelper
 import grails.plugins.elasticsearch.mapping.SearchableClassMappingConfigurator
+import grails.plugins.elasticsearch.util.DomainDynamicMethodsUtils
+import grails.util.Holders
 
 class ElasticsearchBootStrap {
 
     ElasticSearchBootStrapHelper elasticSearchBootStrapHelper
     SearchableClassMappingConfigurator searchableClassMappingConfigurator
 
+    def grailsApplication
+    def applicationContext
     def init = { servletContext ->
+        grailsApplication = Holders.grailsApplication
+        applicationContext = Holders.applicationContext
 
-        //Fixes https://github.com/noamt/elasticsearch-grails-plugin/issues/198
         searchableClassMappingConfigurator.configureAndInstallMappings()
+        DomainDynamicMethodsUtils.injectDynamicMethods(grailsApplication, applicationContext)
         elasticSearchBootStrapHelper?.bulkIndexOnStartup()
     }
 }
